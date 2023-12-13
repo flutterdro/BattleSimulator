970536503caed590cb65eec474d0be540ef3cd8a
Rostyslav <st1tchqwerty@gmail.com>
2023-12-13 21:02:12 +0100

    -1 file

--- Unit.h
+++ Unit.h
@@ -15,7 +15,8 @@ public:
     int maxMove, maxStep; // Movement capabilities
     bool isDeadInside;
     int damage;
-    Unit(const std::string& id, int hp, int posX, int posY, int maxMove, int maxStep, int damage);
+    Unit(const std::string& id, int hp, int posX, int posY, int maxMove, int maxStep, int damage)
+        : id(id), hp(hp), posX(posX), posY(posY), maxMove(maxMove), maxStep(maxStep), isDeadInside(false), damage(damage) {}
 
     virtual ~Unit() = default;
 
